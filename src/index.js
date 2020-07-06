bda0a839839792af32aec8bc3f912424a6080a3d
mhatiboglu <mh1741mh@gmail.com>
2020-07-06 09:40:48 +0300

    Set default configuration for axios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import axios from "axios";
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+axios.defaults.header.common["Authorization"] = "AUTH TOKEN";
+axios.defaults.header.post["Content-Type"] = "application/json";
+
+axios.interceptors.request.use(
+  (request) => {
+    console.log(request);
+    // Edit request config
+    return request;
+  },
+  (error) => {
+    console.log(console.error);
+    return Promise.reject(error);
+  }
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
